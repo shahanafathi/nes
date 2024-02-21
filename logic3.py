4a091cee1d1d417de22501b7eac1ebb4ed201f05
ismail1712 <muhammedismailp12@gmail.com>
2024-02-21 14:48:47 +0530

    'logic3'

---
+++ logic3.py
@@ -0,0 +1,13 @@
+# a=[]
+# # b=[]
+# n=int(input("enter yor range : "))
+# for i in range(n):
+#     f=int(input("enter your number : "))
+#     a.append(f)
+#     a.sort()
+# for i in range(1, n + 1):
+#     if a[i] - a[i - 1] > 1:
+#         print(f" {a[i - 1] + 1}")
+#         break
+# else:
+#       print(f" {a[-1] + 1}")
